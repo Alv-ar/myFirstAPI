5d4795f67e35118136b849d3bdddfc800e72b234
Alv-ar <alvarrr2001@gmail.com>
2021-10-05 19:08:22 +0200

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require(`express`);
 const app = express();
+require('dotenv');
 
 const myFirstController = require(`./controllers/myFirstController`);
 app.get(`/`, myFirstController.helloWorld);
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log(`Example app listening on port 3000!`);
 })
