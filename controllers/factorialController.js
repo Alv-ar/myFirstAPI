3b8f886e7e5aa4b224007dba0d892a97085c3856
Alv-ar <alvarrr2001@gmail.com>
2021-11-02 18:22:58 +0100

    feat: added factorialize

--- controllers/factorialController.js
+++ controllers/factorialController.js
@@ -4,6 +4,16 @@ const status = require("../status/status");
 
 module.exports = {
     getFactorial: (req, res) => {
-
+        const num = req.params.num;
+        function factorialize(num) {
+            if (num < 0) 
+                return -1;
+            else if (num == 0) 
+                return 1;
+            else {
+                return (num * factorialize(num - 1));
+            }
+          }
+        factorialize(num)
     }
 }
